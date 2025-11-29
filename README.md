657d6f0bf20f245fd81f50e4e19d7c78b6d2866e
guilhermeperas <pguilherme926@gmail.com>
2025-11-29 17:37:28 +0000

    small updates

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Number Picker
 
 A simple number guessing game built with HTML, CSS, and JavaScript.
+All front-end was made using AI, while back-end, game logic was me.
 
 ## How can i make it better?
 
--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const form = document.getElementById('number-form');
 const input = document.getElementById('number-input');
 const resultDiv = document.getElementById('result');
+const popup = document.createElement("p");
+const h1 = document.getElementById("title");
+
 let random_value = new_value();
+console.log(random_value);
 
 form.addEventListener('submit', function(event) {
     event.preventDefault()
     var value = parseInt(input.value)
     if(value == random_value){
-        const node = document.createElement("p");
-        node.textContent = "Correct!!!"
-        node.classList.add("correct")
-        resultDiv.appendChild(node)
+        popup.textContent = "Correct!!!"
+        popup.classList.add("correct")
+        h1.textContent = `Pick the correct number from 1 to 10 again!!`;
         random_value = new_value();
-        setTimeout(clear_result, 2000);
+    }else{
+        popup.textContent = "Wrong! Try again."
+        popup.classList.add("wrong")
     }
+    resultDiv.appendChild(popup)
+    setTimeout(clear_result, 1500);
 })
 
 function new_value(){
@@ -21,6 +28,8 @@ function new_value(){
 }
 
 function clear_result(){
+    popup.classList.remove("correct");
+    popup.classList.remove("wrong");
     resultDiv.innerHTML = '';
-    input.value = '';
+    input.focus();
 }
