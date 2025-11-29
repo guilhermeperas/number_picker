51b82580cfadd44f4c2a5509226a8662630a514c
guilhermeperas <pguilherme926@gmail.com>
2025-11-29 17:25:42 +0000

    updated the logic

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const form = document.getElementById('number-form');
 const input = document.getElementById('number-input');
 const resultDiv = document.getElementById('result');
+let random_value = new_value();
 
 form.addEventListener('submit', function(event) {
     event.preventDefault()
     var value = parseInt(input.value)
-    var random_value = Math.floor(Math.random() * 10) + 1;   
     if(value == random_value){
         const node = document.createElement("p");
         node.textContent = "Correct!!!"
         node.classList.add("correct")
         resultDiv.appendChild(node)
+        random_value = new_value();
+        setTimeout(clear_result, 2000);
     }
 })
+
+function new_value(){
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+function clear_result(){
+    resultDiv.innerHTML = '';
+    input.value = '';
+}
